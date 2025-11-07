0cb415532bd8fc9bc3d1c431d7422dce52397070
Vitaliano <dixerior@gmail.com>
2025-11-07 18:23:06 +0200

    dynamic_ip

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::sync::mpsc;
 use crate::agent::Agent;
 use crate::virtual_gamepad::virtual_gamepad::VirtualGamepad;
+use std::net::UdpSocket;
 
 mod virtual_gamepad;
 mod agent;
@@ -17,7 +18,12 @@ async fn main() {
 
     let agent = Agent::new();
     let gamepad_receiver = agent.subscribe();
-    let mut server = WebsocketServer::new("192.168.0.106:9001", agent).await;
+
+    let ip = get_local_ip().unwrap_or("127.0.0.1".to_string());
+    let address = format!("{}:9001", ip);
+    let mut server = WebsocketServer::new(&address, agent).await;
+    println!("✅ WebSocket server started on {}", address);
+
 
     let mut gamepad = VirtualGamepad::new(gamepad_receiver);
 
@@ -35,6 +41,18 @@ async fn main() {
 
 }
 
+
+fn get_local_ip() -> Option<String> {
+    // Створюємо UDP сокет і "підключаємось" до публічної адреси (без фактичного відправлення)
+    if let Ok(socket) = UdpSocket::bind("0.0.0.0:0") {
+        if socket.connect("8.8.8.8:80").is_ok() {
+            if let Ok(local_addr) = socket.local_addr() {
+                return Some(local_addr.ip().to_string());
+            }
+        }
+    }
+    None
+}
 
 // pub async fn read_std_in() -> String {
 //     let stdin = io::stdin();                // отримуємо stdin
